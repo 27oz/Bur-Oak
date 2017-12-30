ee0f3869d8ade8da2e89d7043879a0254f1b1291
Joe Dahle <joe.dahle@gmail.com>
2017-12-29 23:35:26 -0700

    syntax: update to arrow fn

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static("app"));
 
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/app/index.html"));
 });
 
